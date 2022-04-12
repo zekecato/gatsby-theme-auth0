0d62e4cc8f9e39d2695185c81af9ed0c2313ff39
zekecato <53327416+zekecato@users.noreply.github.com>
2022-04-11 22:17:08 -0500

    Update useAuth.js

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,14 +1,16 @@
 import * as React from "react";
 import auth from "../auth/service";
 
-const useAuth = (stateCallback = _state => {}) => {
+const useAuth = (stateCallback = (_state) => {}) => {
   const [isLoading, setIsLoading] = React.useState(true);
   const [isLoggedIn, setIsLoggedIn] = React.useState(auth.isAuthenticated());
   const [profile, setProfile] = React.useState(auth.getUserProfile());
+  const [token, setToken] = React.useState(auth.getAccessToken());
+  const [idToken, setIdToken] = React.useState(auth.getIdToken());
 
   React.useEffect(() => {
     // Override `sessionStateCallback` in auth service
-    auth.sessionStateCallback = state => {
+    auth.sessionStateCallback = (state) => {
       stateCallback(state);
       setIsLoggedIn(state.isLoggedIn);
     };
@@ -18,6 +20,10 @@ const useAuth = (stateCallback = _state => {}) => {
       try {
         const user = auth.getUserProfile();
         setProfile(user);
+        const userToken = auth.getAccessToken();
+        setToken(userToken);
+        const userIdToken = auth.getIdToken();
+        setIdToken(userIdToken);
       } catch (error) {
         console.log(`Error: ${error}`);
       }
@@ -35,6 +41,8 @@ const useAuth = (stateCallback = _state => {}) => {
     isLoading,
     isLoggedIn,
     profile,
+    token,
+    idToken,
   };
 };
 
